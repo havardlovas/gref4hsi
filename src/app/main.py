61ab366df78a967ad36d0fbde4d3125d2462c4a4
havardlovas <havard.s.lovas@ntnu.no>
2023-10-02 06:54:37 +0200

    added __init_.py file to define "scripts" as a package.

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,7 @@
 import configparser
-from ..lib import hyperspectral_utils
-from ..scripts import visualize, georeference
+
+from ..scripts import visualize, georeference, agisoft_extract
+
 # Import the configuration file and read it into dictionary
 home_path = 'C:/Users/haavasl/PycharmProjects/hyperspectral_toolchain'
 data_dir = home_path + '/data/Skogn21012021'
@@ -11,6 +12,8 @@ config.read(config_file)
 
 
 def main():
+    # Extract data from Agisoft
+    agisoft_extract.main(config_file)
     # Visualize the data
     visualize.show_mesh_camera(config)
 
