07d9ad48f147691405f9c2d644a52da47d1aac1d
Havard Lovas <havard.s.lovas@ntnu.no>
2023-11-03 07:18:44 +0100

    Test commit for educational purposes

--- src/tests/test_main_hi.py
+++ src/tests/test_main_hi.py
@@ -3,22 +3,9 @@ import configparser
 import sys
 import os
 
-current_directory = os.getcwd()
-print("Current working directory:", current_directory)
-
 # Local resources
 from scripts import georeference
-# Relative import
 from lib import parsing_utils
-
-
-from tests.test_main_uhi import TestClass
-
-TestClass.say_hello()
-
-
-# Equivalent absolute import
-
 from scripts import visualize
 
 
